62ccd8da6ad03510c662af9f494a6a2f7ff31caa
James Lee <uptownhr@gmail.com>
2019-02-01 03:15:37 -0800

    login logout

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <h1>Hello world</h1>
-
-    <button @click="auth">Login</button>
-    <div id="auth"></div>
+    <pre>{{user}}</pre>
+    <button v-if="!user" @click="auth">Login</button>
+    <button v-else @click="logout">Logout</button>
   </div>
 </template>
 
@@ -11,17 +11,40 @@
   const netlifyIdentity = require('netlify-identity-widget')
 
   export default {
+    data () {
+      return {
+        user: null
+      }
+    },
     mounted () {
-      netlifyIdentity.init({
-        container: '#auth' // defaults to document.body,
-      })
-      console.log('hi')
+      netlifyIdentity.on('init', user => this.login(user))
+      netlifyIdentity.on('login', user => console.log('login', user))
+      netlifyIdentity.on('logout', () => console.log('Logged out'))
+      netlifyIdentity.on('error', err => console.error('Error', err))
+      netlifyIdentity.on('open', () => console.log('Widget opened'))
+      netlifyIdentity.on('close', () => console.log('Widget closed'))
+
+      netlifyIdentity.init()
+      const userx = netlifyIdentity.currentUser()
+
+      console.log('hi', userx)
+
+
+
     },
 
     methods: {
+      login ( user) {
+        console.log('nothing?', this)
+        this.user = user
+      },
       auth () {
         console.log(1)
         netlifyIdentity.open()
+      },
+      logout () {
+        netlifyIdentity.logout()
+        this.user = null
       }
     }
   }
