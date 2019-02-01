8d3f159d5cda83cb4566275c88e45d8842683722
James Lee <uptownhr@gmail.com>
2019-02-01 03:24:14 -0800

    login logout

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -18,7 +18,7 @@
     },
     mounted () {
       netlifyIdentity.on('init', user => this.login(user))
-      netlifyIdentity.on('login', user => console.log('login', user))
+      netlifyIdentity.on('login', user => this.login(user))
       netlifyIdentity.on('logout', () => console.log('Logged out'))
       netlifyIdentity.on('error', err => console.error('Error', err))
       netlifyIdentity.on('open', () => console.log('Widget opened'))
